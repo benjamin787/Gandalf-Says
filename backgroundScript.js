c9e514d4121206ded2c11ce77caf2b7d32d6a6bb
Ben Rowe <brow495@gmail.com>
2021-01-20 12:59:31 -0700

    Change title. Add functionality to fetch from quote API and create notifications.

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,21 +1,42 @@
-//alarm turns on when active tab changes - need way to note inactivity to turn off
-chrome.tabs.onActivated.addListener(() => {
-    chrome.alarms.get('working', alarm => {
-        if (!alarm) {
-            chrome.alarms.create('working', {delayInMinutes: 30, periodInMinutes: 30})
-        }
-    })
+chrome.idle.setDetectionInterval(1500)
+
+chrome.idle.onStateChanged.addListener(newState => {
+    newState === 'active'
+        ? (console.log('alarm on'),
+        chrome.alarms.create('working', {delayInMinutes: 30, periodInMinutes: 30}))
+        : chrome.alarms.clear('working', () => console.log('alarm cleared'))
 })
 
-// chrome.alarms.clear('waiting', () => {
-
-// })
-
-
-
 //notification stuff goes in here
-chrome.alarms.onAlarm.addListener(alarm => {
+chrome.alarms.onAlarm.addListener(async alarm => {
     console.log('alarm!', alarm)
+    try {
+        let quote = await findQuote()
+    } catch(error) {
+        let quote = 'Fix me, Ben! Gosh.'
+    }
+    chrome.notifications.create({
+        message: quote,
+        priority: 2,
+        title: 'Gandalf Says',
+        type: 'basic'
+    })
+    //callback function may be required. unclear
 })
 
 //add browser action to generate quote (notification? or popup in this case?) on click
+
+const findQuote = () => {
+    fetch("https://type.fit/api/quotes")
+    .then(parseJSON)
+    .then(formatNote)
+}
+
+const parseJSON = response => response.json()
+
+const dfhaformatNote = (text, author) => {
+    return `${text}\n   - ${author}`
+}
+const formatNote = (data) => {
+    return `${data.text}\n   - ${data.author}`
+}
