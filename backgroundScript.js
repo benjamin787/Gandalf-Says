631f6a6bb13e045725bf69a3c7f97320303c4075
Ben Rowe <brow495@gmail.com>
2021-01-23 11:14:08 -0700

    Add query params for more control over fetch. Organizie backgroundScript.

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,42 +1,66 @@
-chrome.idle.setDetectionInterval(1500)
-
-chrome.idle.onStateChanged.addListener(newState => {
-    newState === 'active'
-        ? (console.log('alarm on'),
-        chrome.alarms.create('working', {delayInMinutes: 30, periodInMinutes: 30}))
-        : chrome.alarms.clear('working', () => console.log('alarm cleared'))
-})
-
-//notification stuff goes in here
-chrome.alarms.onAlarm.addListener(async alarm => {
-    console.log('alarm!', alarm)
-    try {
-        let quote = await findQuote()
-    } catch(error) {
-        let quote = 'Fix me, Ben! Gosh.'
-    }
+const sendNote = quote => {
+    console.log('sendNote hit')
     chrome.notifications.create({
-        message: quote,
-        priority: 2,
         title: 'Gandalf Says',
+        message: quote,
+        iconUrl: '/gandalf.jpg',
+        priority: 2,
         type: 'basic'
-    })
-    //callback function may be required. unclear
-})
+    }, notificationId => notificationId
+    )
+}
 
-//add browser action to generate quote (notification? or popup in this case?) on click
-
-const findQuote = () => {
-    fetch("https://type.fit/api/quotes")
-    .then(parseJSON)
-    .then(formatNote)
+const formatNote = (data) => {
+    console.log('formatNote hit. here is data: ', data)
+    return `${data.text}\n   - ${data.author}`
 }
 
 const parseJSON = response => response.json()
 
-const dfhaformatNote = (text, author) => {
-    return `${text}\n   - ${author}`
+
+const findQuote = () => {
+    let i = Math.floor(Math.random() * 1643)
+    console.log('findQuote fired. i = ', i)
+    fetch(`https://type.fit/api/quotes?limit=1&offset=${i}`)
+    .then(parseJSON)
+    .then(formatNote)
+    .then(sendNote)
 }
-const formatNote = (data) => {
-    return `${data.text}\n   - ${data.author}`
+
+const stopAlarm = newState => {
+    if (newState !== 'active')
+        chrome.alarms.clear('working', () => console.log('alarm cleared'))
 }
+
+const startAlarm = () => {
+    chrome.alarms.create('working', {delayInMinutes: 30, periodInMinutes: 30})
+    console.log('alarm created')
+}
+
+const checkAlarm = activeInfo => {
+
+    chrome.alarms.get('working', alarm => alarm ? console.log('alarm found') : startAlarm())
+}
+
+console.log('hi')
+
+chrome.tabs.onActivated.addListener(checkAlarm)
+
+chrome.idle.setDetectionInterval(1500)
+
+chrome.idle.onStateChanged.addListener(stopAlarm)
+
+chrome.alarms.onAlarm.addListener(findQuote)
+
+// chrome.alarms.onAlarm.addListener(async alarm => {
+//     let quote = await findQuote()
+//     console.log('QUOTE IS: ', quote)
+
+//     chrome.notifications.create({
+//         title: 'Gandalf Says',
+//         message: quote,
+//         iconUrl: '/gandalf.jpg',
+//         priority: 2,
+//         type: 'basic'
+//     })
+// })
